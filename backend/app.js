5b21d8488ebec89dbf8b577f829916edf5e08c4e
garima <saxenagarima2953@gmail.com>
2025-06-02 16:32:37 +0530

    update CORS configuration

--- backend/app.js
+++ backend/app.js
@@ -6,11 +6,11 @@ const app = express();
 
 // Middlewares
 app.use(cors({
-    origin: 'http://localhost:5173', // frontend origin
-    credentials: true
-  }));
-app.use(express.json({limit : '16kb'}));
-app.use(express.urlencoded({extended: true, limit : '16kb'}));
+  origin: ['https://taskora-three.vercel.app', 'http://localhost:5173'], // frontend origin
+  credentials: true
+}));
+app.use(express.json({ limit: '16kb' }));
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(cookieParser());
 
 
@@ -22,7 +22,7 @@ import taskRoute from './src/routes/task.route.js';
 import searchRoute from './src/routes/search.route.js';
 
 app.get('/api/v1', (req, res) => {
-    res.send( 'Welcome to the Kanban Board API' );
+  res.send('Welcome to the Kanban Board API');
 });
 
 app.use('/api/v1/auth', authRoute);
